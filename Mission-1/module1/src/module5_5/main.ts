1647c02aebad114dfc911009aa38496d4b4c8c0f
nizam754 <m.nizam754@gmail.com>
2023-05-11 20:30:21 +0600

    5.5

--- Mission-1/module1/src/module5_5/main.ts
+++ Mission-1/module1/src/module5_5/main.ts
@@ -1,7 +1,11 @@
-import * as Methods from "./module"
+// import jekono from "./module"
+import addTwo from './utils/add'
+import subtract from './utils/subtract'
+import multiply from './utils/multiply'
+import divide from './utils/divide'
+
 const add = (param1: number, param2: number, param3: number): number => {
     return param1 + param2 + param3
 };
 
-const res = Methods.add(4, 6);
-const res = Methods.average(4, 6);
+const res = addTwo(4, 6);
--- Mission-1/module1/src/module5_5/module.ts
+++ Mission-1/module1/src/module5_5/module.ts
@@ -1,19 +1,7 @@
-export const add =(param1:number, param2:number):number=>{
-    return param1 + param2
-}
-
-export const subtract=(param1:number, param2:number):number=>{
-    return param1 - param2
-}
-
-export const multiply=(param1:number, param2:number):number=>{
-    return param1 * param2
-}
-
-export const divide=(param1:number, param2:number):number=>{
-    return param1 / param2
-}
-
-export const average=(param1:number, param2:number):number=>{
-    return (param1 + param2)/2
-}
+/* export default {
+    add,
+    subtract,
+    multiply,
+    divide,
+    average,
+}; */
