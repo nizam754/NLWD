129d6e53515fd7b05a2bddfa2a016f0b95fadb39
Mostafa Nizam <m.nizam754@gmail.com>
2023-05-11 10:17:26 +0600

    main

--- Mission-1/module1/src/module5_5/main.ts
+++ Mission-1/module1/src/module5_5/main.ts
@@ -1,6 +1,7 @@
-import {add as addTow, subtract} from "./module"
+import * as Methods from "./module"
 const add = (param1: number, param2: number, param3: number): number => {
     return param1 + param2 + param3
 };
 
-const res = addTow(4, 6);
+const res = Methods.add(4, 6);
+const res = Methods.average(4, 6);
--- Mission-1/module1/src/module5_5/module.ts
+++ Mission-1/module1/src/module5_5/module.ts
@@ -1,7 +1,19 @@
-export const add =(param1:number, param2:number):number{
+export const add =(param1:number, param2:number):number=>{
     return param1 + param2
 }
 
-export const subtract=(param1:number, param2:number):number{
+export const subtract=(param1:number, param2:number):number=>{
     return param1 - param2
+}
+
+export const multiply=(param1:number, param2:number):number=>{
+    return param1 * param2
+}
+
+export const divide=(param1:number, param2:number):number=>{
+    return param1 / param2
+}
+
+export const average=(param1:number, param2:number):number=>{
+    return (param1 + param2)/2
 }
