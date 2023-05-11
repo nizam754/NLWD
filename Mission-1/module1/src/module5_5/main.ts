d92965f8d08fb61c952f72d92e1e5430c1b4b7d6
Mostafa Nizam <m.nizam754@gmail.com>
2023-05-11 08:09:24 +0600

    module5_5

---
+++ Mission-1/module1/src/module5_5/main.ts
@@ -0,0 +1,6 @@
+import {add as addTow} from "./module"
+const add = (param1: number, param2: number, param3: number): number => {
+    return param1 + param2 + param3
+};
+
+const res = addTow(4, 6);
