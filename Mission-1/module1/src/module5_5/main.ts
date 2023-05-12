a5621ae385c995e1272a1486df19d2baa2f0bc99
Mostafa Nizam <m.nizam754@gmail.com>
2023-05-12 09:32:05 +0600

    5-5.2: Different types Import and export statement

--- Mission-1/module1/src/module5_5/main.ts
+++ Mission-1/module1/src/module5_5/main.ts
@@ -1,11 +1,17 @@
 // import jekono from "./module"
-import addTwo from './utils/add'
-import subtract from './utils/subtract'
-import multiply from './utils/multiply'
-import divide from './utils/divide'
+// import addTwo from './utils/add'
+// import subtract from './utils/subtract'
+// import multiply from './utils/multiply'
+// import divide from './utils/divide'
+// import average from './utils/average'
+import methods from './utils/index'
 
 const add = (param1: number, param2: number, param3: number): number => {
     return param1 + param2 + param3
 };
 
-const res = addTwo(4, 6);
+const res1 = methods.addTwo(4, 6);
+const res2 = methods.subtract(4, 6);
+const res3 = methods.multiply(4, 6);
+const res4 = methods.divide(4, 6);
+const res5 = methods.average(4, 6);
---
+++ Mission-1/module1/src/module5_5/utils/index.ts
@@ -0,0 +1,13 @@
+import addTwo from './add';
+import subtract from './subtract';
+import multiply from './multiply';
+import divide from './divide';
+import average  from './average';
+
+export default{
+    addTwo,
+    subtract,
+    multiply,
+    divide,
+    average
+}
